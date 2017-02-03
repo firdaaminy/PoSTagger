4d1ec40a5dadf3bc4d9fa6891324f22926e1bf41
hizas-code <hizassr@gmail.com>
2017-02-03 22:24:21 +0700

    collecting tag

--- hmm.py
+++ hmm.py
@@ -1,3 +1,11 @@
 
 data_train = "pos.train.txt"
 data_test = "pos.test.txt"
+list_tag = []
+
+with open(data_train) as data :
+    for datum in data :
+        tag = datum.rsplit(' ',1)[-1]
+        if not tag in list_tag :
+            list_tag.append(tag)
+            print(tag)
