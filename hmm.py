421dff7b653d0f775ff52dd30fb6f06ab64d7f07
hizas-code <hizassr@gmail.com>
2017-02-05 21:41:54 +0700

    learn-param 50%

--- hmm.py
+++ hmm.py
@@ -9,21 +9,34 @@ list_tag = [
 
 with open(data_train) as data :
     backpointer = "<s>"
-    #print(list_tag[1][0])
     for datum in data :
-        word = datum.rsplit(' ',1)[0]
-        tag = datum.rsplit(' ',1)[-1]
-        if tag in list_tag[0] :
+        word = datum.rsplit(' ',-1)[0] # ngambil word dari line
+        tag = datum.rsplit(' ',-1)[-1] # ngambil tag dari line
+        tag = tag.replace("\n","")
+        if datum != "" :
+            if not tag in list_tag[0] :
+                list_tag[0].append(tag)
+                list_tag[1].append({})
+                list_tag[2].append({})
+
             x = list_tag[0].index(backpointer)
-            if tag in list_tag[1][x] :
+            if tag in list_tag[1][x]:
                 list_tag[1][x][tag] += 1
+            else:
+                list_tag[1][x][tag] = 1
+
+            if word in list_tag[2] :
+                y = list_tag[2][x][word]
+                if backpointer in y :
+                    y[backpointer] += 1
+                else :
+                    y[backpointer] = 1
             else :
-                list_tag[1][x][tag] = []
-        else :
-            list_tag[0].append(tag)
-            list_tag[1].append({})
-            list_tag[2].append({})
-        backpointer = tag
+                list_tag[2][x][backpointer] = {}
 
+            backpointer = tag
 
-    print(list_tag)
+    #print(list_tag)
+    for x in list_tag[1] :
+        print(x)
+
