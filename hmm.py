848fae0e1ec8c421e66a479a47b4028c4e9bbcbf
hizas-code <hizassr@gmail.com>
2017-02-05 19:47:41 +0700

    progress 1

--- hmm.py
+++ hmm.py
@@ -1,11 +1,29 @@
 
 data_train = "pos.train.txt"
 data_test = "pos.test.txt"
-list_tag = []
+list_tag = [
+    ["<s>", "<e>"], #tags
+    [{},{}], #transition prob
+    [{},{}] #emmision prob
+]
 
 with open(data_train) as data :
+    backpointer = "<s>"
+    #print(list_tag[1][0])
     for datum in data :
+        word = datum.rsplit(' ',1)[0]
         tag = datum.rsplit(' ',1)[-1]
-        if not tag in list_tag :
-            list_tag.append(tag)
-            print(tag)
+        if tag in list_tag[0] :
+            x = list_tag[0].index(backpointer)
+            if tag in list_tag[1][x] :
+                list_tag[1][x][tag] += 1
+            else :
+                list_tag[1][x][tag] = []
+        else :
+            list_tag[0].append(tag)
+            list_tag[1].append({})
+            list_tag[2].append({})
+        backpointer = tag
+
+
+    print(list_tag)
